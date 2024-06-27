81f895d97802760d8206649d7eadbf065839751c
adarshzpatel <adarshzpatel@gmail.com>
2024-06-27 07:44:47 +0530

    change default port for staging

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { Commitment, Keypair } from "@solana/web3.js";
 export const RPC_URL = "https://devnet.helius-rpc.com/?api-key=5163c3d1-8082-442e-8a15-c27bff3cfabb";
 export const PROGRAM_ID = "33ZENzbUfMGwZZYQDCj8DEeBKBqd8LaCKnMfQQnMVGFW";
 export const COMMITMENT: Commitment = "processed";
-export const WS_PORT = (process.env.PORT || 8080) as number;
+export const WS_PORT = (process.env.PORT || 3000) as number;
 export const OWNER_KEYPAIR = Keypair.fromSecretKey(
   Uint8Array.from([
     1, 60, 46, 125, 82, 22, 178, 15, 93, 247, 249, 207, 76, 156, 177, 42, 124,
